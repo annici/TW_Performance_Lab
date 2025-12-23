aef626b3981c21f110c240e0944987009547a6ac
annigit <annushka@mail.com>
2025-12-23 20:45:01 +0300

    task2 - with no dots err task3 - full

---
+++ task3/task3.py
@@ -0,0 +1,33 @@
+import json
+import shutil
+
+#распаковываем файл с оценками и применяем json разметку
+with open('values.txt',  'r', encoding='Utf-8') as values:
+    values_loaded = json.load(values)['values'] #list
+    values_as_dict={}
+    for i in values_loaded: #формируем словарик типа "айди: оценка"
+        values_as_dict |= {i['id']:i['value']}
+
+with open('tests.txt',  'r', encoding='Utf-8') as tests:
+    tests_loaded = json.load(tests)
+
+
+def iter_thrgh_dict(report_as_list,values_as_dict): #на вход list с тестами и dict с оценками
+
+    for i_dict in report_as_list: #итерируем по словарям в списке
+        if 'value' in  i_dict:
+            i_dict['value']=values_as_dict[i_dict['id']]
+        if 'values' in i_dict:
+                try: # заходим в новый уровень вложенности и опять итерируем по словарям в новом списке
+                    iter_thrgh_dict(i_dict['values'],values_as_dict)
+                except KeyError as ww:
+                    print(f"опять проблема с {ww}")
+    return report_as_list #list
+
+edited_tests=iter_thrgh_dict(tests_loaded['tests'],values_as_dict)
+
+#в файл 'report' <- отредактированный 'tests':
+with open('report.json', 'w') as file:
+    json.dump({'tests':edited_tests}, file, indent=4)
+
+
